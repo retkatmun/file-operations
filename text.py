6a663dcef121c4df64f8d2a587257c4a3478192c
Retkatmun <retkatmun1@gmail.com>
2025-07-19 16:59:39 +0000

    i added some little calculation

--- text.py
+++ text.py
@@ -1,3 +1,4 @@
+'''
 file = open("sample.txt")
 file_sample = file.read()
 file.close
@@ -8,6 +9,18 @@ file = open("sample.txt", "a")
 file_sample = file.write("good job\n")
 file.close()
 print(file_sample)
+'''
+
+one1 = open("one.txt")
+one1_txt = one1.read()
+one1.close()
+
+two2 = open("two.txt")
+two2_txt = two2.read()
+two2.close()
+
+print(int(one1_txt) + int(two2_txt))
+
 
 '''
 file = open("sample.txt", "w")
